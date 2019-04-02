b1ff4595c4e0e5ac436dfaeabb8862a76da92ae4
mbgeorge48 <mbgeorge48@gmail.com>
2019-04-03 00:10:15 +0100

    doing a bit of tidying up, added a readme and continued processing the nicknames

---
+++ README.md
@@ -0,0 +1,5 @@
+# Facebook Messenger Nickname Parser
+
+This is a simple script that is designed to read in a json dump of a Facebook messenger group chat then pull the nicknames out so you can see who's been called what
+
+It's supposed to be a bit of fun between me and some friends but thought I'd share it incase anyone else fancied using it
--- main.py
+++ main.py
@@ -20,51 +20,47 @@ def get_participants(json_string):
 def get_nicknames(json_string, participants):
     main_pattern = '^.*?set (his own|her own|the|your) nickname.*?$'
 
-    nickname_patterns = {
+    nickname_patterns = { #Need one for one's you set your self
         "someone_set_your_nickname": "^.*?set your nickname to .*?",
         "you_set_someones_nickname": "^You set the nickname for .*?",
         "someone_set_their_own_nickname": "^.*?set (his own|her own) nickname to .*?",
         "someone_set_someones_nickname": "^.*?set the nickname for .*?"
     }
 
-    nickname_list = list()
-    nickname_updates = dict()
-
+    nickname_details = dict()
     for message in json_string:
         try:  # Need to catch as I found that not all messages have content
             if re.match(main_pattern, message["content"]) is not None:
-                nickname_list.append(message['content'])
+                nickname_details[message['content']] = message['timestamp']
         except:
             pass
 
-# Make 4 lists then send them back
-# 4 methods (one each) to parse seperatly
-
+    # Matches the 4 regex patterns in "nickname_patterns"
+    # https://docs.python.org/2/library/collections.html#collections.defaultdict rather than 4 lists?
     nickname_match_a = list()
     nickname_match_b = list()
     nickname_match_c = list()
     nickname_match_d = list()
 
-   for update in nickname_list:
-        if re.match(nickname_patterns.get("someone_set_your_nickname"), update) is not None:
-            nickname_updates["someone_set_your_nickname"] = update
-        # elif re.match(nickname_patterns.get("you_set_someones_nickname"), update) is not None:
+    for item in list(nickname_details.items()):
+        if re.match(nickname_patterns.get("someone_set_your_nickname"), item.key()) is not None:
+            nickname_match_a.append(item.key())
+        elif re.match(nickname_patterns.get("you_set_someones_nickname"), item) is not None:
+            nickname_match_b.append(item.key())
+        elif re.match(nickname_patterns.get("someone_set_their_own_nickname"), item) is not None:
+            nickname_match_c.append(item.key())
+        elif re.match(nickname_patterns.get("someone_set_someones_nickname"), item) is not None:
+            nickname_match_d.append(item.key())
 
-        # for pattern in nickname_patterns.values():
-        #     if re.match(pattern, update) is not None:
-        #         print (pattern, update)
-    print(nickname_updates["someone_set_your_nickname"])
+    return "None"
 
 
 def main():
     json_string = load_json()
     participants = get_participants(json_string['participants'])
-    get_nicknames(json_string['messages'], participants)
-
-    # for message in json_string["messages"]:
-    #     # print (message)
-    #     if "set " in message['content']:
-    #         print (message)
+    all_nicknames = get_nicknames(json_string['messages'], participants)
+    # once all the nicknames are returned search the values for participants
+    # after that you need to pull the nickname out 
 
 
 if __name__ == '__main__':

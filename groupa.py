6538962b31103e6d9afb41a27586302185fa3e9d
Matt George <mbgeorge48@gmail.com>
2022-12-22 22:53:45 +0000

    Reformatting

--- groupa.py
+++ groupa.py
@@ -4,7 +4,7 @@ import os
 import re
 import sys
 
-DIR="/Users/mg/Documents/FB/merged/messages/inbox"
+from utils import encode_string, load_json
 
 def main(dirs_to_group):
     for root, dirs, _ in os.walk(dirs_to_group, topdown=False):
@@ -13,25 +13,30 @@ def main(dirs_to_group):
             this_groups_message_data = {}
             for file in os.listdir(full_path):
                 if re.search("^message.*?.json$", file):
-                    json_file = open(os.path.join(full_path,file))
-                    json_data = json.load(json_file)
-                    json_file.close()
+                    json_string = load_json(os.path.join(full_path,file))
                     if not this_groups_message_data:
-                        this_groups_message_data = json_data
+                        this_groups_message_data = json_string
                     else:
                         participants_list = this_groups_message_data["participants"]
-                        for participant in json_data["participants"]:
+                        for participant in json_string["participants"]:
                             if participant not in participants_list:
                                 participants_list.append(participant)
                         this_groups_message_data.update({'participants': participants_list })
 
-                        message_list = json_data["messages"]+this_groups_message_data["messages"]
-                        sorted_messages = sorted(message_list, key=itemgetter('timestamp_ms'))
-                        this_groups_message_data.update({'messages': sorted_messages })
+                        this_groups_message_data.update({'messages': this_groups_message_data["messages"].extend(json_string["messages"]) })
+
 
             if this_groups_message_data:
+                this_groups_message_data.update({'title':encode_string(this_groups_message_data['title'])})
+                for message in this_groups_message_data['messages']:
+                    try:
+                        message['content'] = encode_string(message['content'])
+                    except:
+                        continue
+                this_groups_message_data.update({'messages': sorted(this_groups_message_data['messages'], key=itemgetter('timestamp_ms')) })
+
                 with open(os.path.join(full_path,'combined_messages.json'), 'w') as f:
-                    json.dump(this_groups_message_data, f, indent=4)
+                    json.dump(this_groups_message_data, f, indent=4, ensure_ascii=False)
                     print(os.path.join(full_path,'combined_messages.json'))
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -4,3 +4,6 @@ def load_json(file_to_parse):
     with open(file_to_parse) as json_data:
         json_string = json.load(json_data)
     return json_string
+
+def encode_string(string):
+    return string.encode('latin1').decode('utf8')

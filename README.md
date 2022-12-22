3641b7fe8ee5bc9e3aa4a9d00ad26826384bff33
Matt George <mbgeorge48@gmail.com>
2022-12-22 18:32:13 +0000

    Add the Unzippa to unzip and merge facebook exports

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 This is a simple script that is designed to read in a json dump of a Facebook messenger group chat then pull the nicknames out so you can see who's been called what
 
 It's supposed to be a bit of fun between me and some friends but thought I'd share it incase anyone else fancied using it
+
+## Unzippa
+
+When you do export your data from Facebook it gives it to you in a tonne of smallish (around 2 to 3 gb) zips files
+This script unzips them all and merges them all into one folder
+Then it can rezip up the merged folder and delete the extra stuff it created along the way
---
+++ unzippa.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function check_space {
+    for zip in $(find . -type f -name "*.zip"); do
+        ((total_space += $(unzip -Zt $zip | awk '{print $6}')))
+    done
+}
+
+function unzip_time {
+    mkdir -p unzipped
+    # If the total uncompressed space is less than 70% of the remaining disk space don't start unzipping
+    if [ $((DISK_SPACE * 70 / 100)) -gt $total_space ]; then
+        for zip in $(find . -type f -name "*.zip"); do
+            FILE=${zip%.zip*}
+            echo "Unzipping $FILE"
+            unzip -q $zip -d unzipped/$FILE
+        done
+    fi
+}
+
+function merging_time {
+    mkdir -p merged
+    for folder in unzipped/*; do
+        echo "Copying $folder"
+        cp -R $folder/ merged
+    done
+}
+
+function rezip {
+    zip -r merged-export.zip merged/*
+}
+
+function tidy_up {
+    rm -rf merged unzipped
+}
+
+# Would need to be tweaked to make it work on Linux
+DISK_SPACE=$(diskutil info /dev/disk3s1s1 | grep 'Container Free Space' | awk '{print substr($6,2) }')
+
+[[ -z "$1" ]] && {
+    echo "Pass in a directory to scan, i.e. $HOME/Documents/folder"
+    exit 1
+}
+cd $1
+total_space=0
+
+# Comment and uncomment as you go to only run certain functions
+check_space
+unzip_time
+merging_time
+rezip
+# tidy_up

21d727b1be37fe5ea76a7461af9c57e5efe2ac64
Matt George <mbgeorge48@gmail.com>
2022-12-25 00:01:46 +0000

    Make a couple tweaks to the message data

--- groupa.py
+++ groupa.py
@@ -1,3 +1,4 @@
+import datetime
 from operator import itemgetter
 import json
 import os
@@ -17,8 +18,8 @@ def main(dirs_to_group):
                     if not this_groups_message_data:
                         this_groups_message_data = json_string
                     else:
-                        participants_list = this_groups_message_data["participants"]
-                        for participant in json_string["participants"]:
+                        participants_list = this_groups_message_data.get("participants")
+                        for participant in json_string.get("participants"):
                             if participant not in participants_list:
                                 participants_list.append(participant)
                         this_groups_message_data.update({'participants': participants_list })
@@ -30,20 +31,20 @@ def main(dirs_to_group):
                 this_groups_message_data.update({'title':encode_string(this_groups_message_data['title'])})
                 for message in this_groups_message_data['messages']:
                     try:
-                        message['content'] = encode_string(message['content'])
+                        message['content'] = encode_string(message.get('content'))
+                        if message.get("reactions"):
+                            for reaction in message.get("reactions"):
+                                reaction["reaction"] = encode_string(reaction.get('reaction'))
+                        if message.get("audio_files"):
+                            message["audio"] = message.get("audio_files")
+                        message["timestamp_converted"] = datetime.datetime.fromtimestamp(message['timestamp_ms']/1000).strftime('%Y-%m-%d %H:%M:%S')
                     except:
                         continue
-                for message in this_groups_message_data['messages']:
-                    try:
-                        for photo in message['photos']:
-                            photo['uri'] = '/'.join(photo['uri'].split('/')[-2:])
-                    except:
-                        continue
-                this_groups_message_data.update({'messages': sorted(this_groups_message_data['messages'], key=itemgetter('timestamp_ms')) })
-
+                this_groups_message_data.update({'messages': sorted(this_groups_message_data.get('messages'), key=itemgetter('timestamp_ms')) })
+                print(this_groups_message_data.get('title'))
                 with open(os.path.join(full_path,'combined_messages.json'), 'w') as f:
                     json.dump(this_groups_message_data, f, indent=4, ensure_ascii=False)
-                    print(os.path.join(full_path,'combined_messages.json'))
+
 
 if __name__ == '__main__':
     if os.path.isdir(sys.argv[1]):

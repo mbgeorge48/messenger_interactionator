ff4ed408e3f1b96c15e3cdfc4b91a412fd2c6520
Matt George <mbgeorge48@gmail.com>
2022-12-25 00:07:38 +0000

    Update the script process all the media

--- photo_sender.py
+++ photo_sender.py
@@ -7,20 +7,38 @@ import os
 
 from utils import load_json
 
-def main(file_to_parse):
-    json_string = load_json(file_to_parse)
-    photo_messages = []
-    for message in json_string["messages"]:
-        try:
 
-            photo_messages.append({message['sender_name']: message['photos']})
-        except:
-            continue
-    print(json.dumps(photo_messages, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False))
+def main(dir_to_scan, media_type):
+    messages_path = os.path.join(dir_to_scan, "combined_messages.json")
+    json_string = load_json(messages_path)
+    messages = json_string["messages"]
 
+    media_path = os.path.join(dir_to_scan, media_type)
+    media_files = os.listdir(media_path)
+
+    updated_messages = []
+    for message in messages:
+        if message.get(media_type):
+            for media_index, media in enumerate(message.get(media_type)):
+                file = (os.path.split(media.get('uri'))[1])
+                if file in media_files:
+                    _, file_extension = os.path.splitext(file)
+                    new_name = f"{datetime.datetime.fromtimestamp(message['timestamp_ms']/1000).strftime('%Y-%m-%d_%H%M%S')}-{message['sender_name'].replace(' ', '_')}{file_extension}"
+                    os.rename(os.path.join(dir_to_scan, media_type, file), os.path.join(dir_to_scan, media_type, new_name))
+                    message.get(media_type)[media_index]['uri']=os.path.join(media_type, new_name)
+                    updated_messages.append(message)
+        else:
+            updated_messages.append(message)
+
+    json_string.update({'messages': updated_messages})
+
+    with open(messages_path, 'w') as f:
+        json.dump(json_string, f, indent=4, ensure_ascii=False)
+        print(messages_path)
 
 if __name__ == '__main__':
-    if os.path.isfile(sys.argv[1]):
-        main(sys.argv[1])
+    if os.path.isdir(sys.argv[1]):
+        for media_type in ['photos', 'videos', 'files', 'audio']:
+            main(sys.argv[1], media_type)
     else:
-        print('Missing path to file')
+        print('Missing path to dir')

636d7cb7101198bd6c2e36ad5c33dec63bbe674d
Matt George <mbgeorge48@gmail.com>
2022-12-26 19:00:28 +0000

    Move audio_files key, add timestamp_converted field and does better error handling

--- groupa.py
+++ groupa.py
@@ -30,16 +30,18 @@ def main(dirs_to_group):
             if this_groups_message_data:
                 this_groups_message_data.update({'title':encode_string(this_groups_message_data['title'])})
                 for message in this_groups_message_data['messages']:
-                    try:
+
+                    if message.get("content"):
                         message['content'] = encode_string(message.get('content'))
-                        if message.get("reactions"):
-                            for reaction in message.get("reactions"):
-                                reaction["reaction"] = encode_string(reaction.get('reaction'))
-                        if message.get("audio_files"):
-                            message["audio"] = message.get("audio_files")
-                        message["timestamp_converted"] = datetime.datetime.fromtimestamp(message['timestamp_ms']/1000).strftime('%Y-%m-%d %H:%M:%S')
-                    except:
-                        continue
+                    if message.get("bumped_message_metadata") and message.get("bumped_message_metadata").get("bumped_message") :
+                        message["bumped_message_metadata"]["bumped_message"] = encode_string(message.get('bumped_message_metadata').get("bumped_message"))
+                    if message.get("reactions"):
+                        for reaction in message.get("reactions"):
+                            reaction["reaction"] = encode_string(reaction.get('reaction'))
+                    if message.get("audio_files"):
+                        message["audio"] = message.pop("audio_files")
+                    message["timestamp_converted"] = datetime.datetime.fromtimestamp(message['timestamp_ms']/1000).strftime('%Y-%m-%d %H:%M:%S')
+
                 this_groups_message_data.update({'messages': sorted(this_groups_message_data.get('messages'), key=itemgetter('timestamp_ms')) })
                 print(this_groups_message_data.get('title'))
                 with open(os.path.join(full_path,'combined_messages.json'), 'w') as f:

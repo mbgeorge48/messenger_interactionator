ec1a6a4bbfa80e7603986386a175f2bef98467d2
Matt George <mbgeorge48@gmail.com>
2023-01-16 14:19:38 +0000

    Add alternate conditional to unzipping

--- unzippa.sh
+++ unzippa.sh
@@ -10,17 +10,23 @@ function check_space {
 function unzip_time {
     mkdir -p unzipped
     # If the total uncompressed space is less than 70% of the remaining disk space don't start unzipping
-    if [ $((DISK_SPACE * 70 / 100)) -gt $total_space ]; then
+    if [ $((DISK_SPACE * 90 / 100)) -gt $total_space ] || [ IGNORE_SPACE_CHECK = true ] then
         echo "Starting to unzip"
+        cd /Volumes/Camera\ Roll/fb/
         for zip in $(find . -type f -name "*.zip"); do
             FILE=${zip%.zip*}
             echo "Unzipping $FILE"
-            unzip -q $zip -d unzipped/$FILE
+            unzip -q $zip -d /Volumes/Camera\ Roll/fb/unzipped/$FILE
         done
+    else
+        echo $DISK_SPACE
+        echo $total_space
+        echo "Not enough disk space"
     fi
 }
 
 function merging_time {
+    cd /Volumes/Camera\ Roll/fb/
     mkdir -p merged
     echo "Starting the merge"
     for folder in unzipped/*; do
@@ -41,6 +47,7 @@ function tidy_up {
 
 # Would need to be tweaked to make it work on Linux
 DISK_SPACE=$(diskutil info /dev/disk3s1s1 | grep 'Container Free Space' | awk '{print substr($6,2) }')
+IGNORE_SPACE_CHECK=true
 
 [[ -z "$1" ]] && {
     echo "Pass in a directory to scan, i.e. $HOME/Documents/folder"
@@ -52,7 +59,7 @@ total_space=0
 
 # Comment and uncomment as you go to only run certain functions
 check_space
-unzip_time
-merging_time
+# unzip_time
+# merging_time
 # rezip
 # tidy_up

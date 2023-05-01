36e248a70dfb4395df188abc0fe916020695eee9
Matt George <mbgeorge48@gmail.com>
2023-05-01 23:10:58 +0100

    Implement some of the functions to get the reactions

--- src/rg_reactionator.py
+++ src/rg_reactionator.py
@@ -4,33 +4,45 @@ import collections
 import datetime
 import sys
 import os
+import operator
 
-from src.utils import load_json, get_participants
+from utils import get_data_to_parse, initial_file_load, write_to_file
 
 YOUR_NAME = "Matt George"
 
 
-def get_all_reacted_messages(json_string):
-    reacted_messages=""
+def get_all_reacted_messages(messages, min_reactions_length=0):
+    all_reacted_messages=[]
+    for message in messages:
+        if "reactions" in message and len(message["reactions"])>min_reactions_length:
+            message["reactions_length"]=len(message["reactions"])
+            all_reacted_messages.append(message)
+    return sorted(all_reacted_messages, key=operator.itemgetter('reactions_length'), reverse=True)
 
-    return reacted_messages
-
-def sort_reacts(all_reacted_messages, participants):
-    reacted_messages=""
-    # Sort into number of reacts in decending order
-    return reacted_messages
 
 def find_most_and_least_reactive_participant(all_reacted_messages, participants):
-    return ""
+    times_reacted={}
+    for participant in participants:
+        times_reacted[participant]=0
+        for message in all_reacted_messages:
+            for reaction in message["reactions"]:
+                if reaction["actor"] == participant:
+                    times_reacted[participant]=times_reacted[participant]+1
+
+    times_reacted = dict(sorted(times_reacted.items(), key=lambda item: item[1],reverse=True))
+    return times_reacted
+
 
 def find_most_likely_to_react_with(all_reacted_messages, participants, emojis):
     return ""
 
 
-def main(file_to_parse):
-    json_string = load_json(file_to_parse)
-    participants = get_participants(json_string['participants'])
-    all_reacted_messages = sort_reacts(get_all_reacted_messages(json_string['messages']), participants)
+def main(data_to_parse):
+    messages, participants=get_data_to_parse(data_to_parse)
+    reaction_data={}
+    reaction_data['reacted_messages'] = get_all_reacted_messages(messages)
+    reaction_data['times_reacted']=find_most_and_least_reactive_participant(reaction_data['reacted_messages'], participants)
+    write_to_file("test.json", reaction_data)
 
     # Pull of the most/least reactive person
     # Run with the common emojis (laugh(all types), shock, heart)
@@ -40,7 +52,7 @@ def main(file_to_parse):
 
 
 if __name__ == '__main__':
-    if os.path.isfile(sys.argv[1]):
-        main(sys.argv[1])
+    if len(sys.argv)>1:
+        main(initial_file_load(sys.argv[1]))
     else:
         print('Missing path to file')
--- src/utils/get_data_to_parse.py
+++ src/utils/get_data_to_parse.py
@@ -1,13 +1,15 @@
-from utils import get_participants, read_message_file
+import os
+import utils
 
 
 def get_data_to_parse(data_to_parse):
     data=[]
-    for file in data_to_parse:
-        data.append(read_message_file(file))
 
+    for file in data_to_parse:
+        data.append(utils.read_message_file(file))
+    participants = messages = []
     for entry in data:
-        participants=participants + get_participants(entry["participants"])
+        participants=participants + utils.get_participants(entry["participants"])
         messages=messages + entry["messages"]
     participants=list(dict.fromkeys(participants))
     return messages, participants

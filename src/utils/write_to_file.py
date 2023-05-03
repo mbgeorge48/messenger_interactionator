23b17282861857b9adf6769b898537f7dd62ad83
Matt George <mbgeorge48@gmail.com>
2023-05-03 23:51:15 +0100

    Add log message

--- src/utils/write_to_file.py
+++ src/utils/write_to_file.py
@@ -4,3 +4,4 @@ import json
 def write_to_file(file_name, data):
     with open(file_name, "w") as f:
         json.dump(data, f, indent=4, separators=(",", ": "), ensure_ascii=False)
+    print(f"writen to {file_name}")

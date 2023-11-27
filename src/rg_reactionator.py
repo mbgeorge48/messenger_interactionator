b04257e12afa534a730188361caa6c3f2440a25f
Matt George <mbgeorge48@gmail.com>
2023-11-27 21:54:32 +0000

    Refactoring

--- src/rg_reactionator.py
+++ src/rg_reactionator.py
@@ -14,22 +14,22 @@ from utils import (
 def get_all_reacted_messages(messages, min_reactions_length=0):
     all_reacted_messages = []
     for message in messages:
-        if "reactions" in message and len(message["reactions"]) > min_reactions_length:
+        if len(message.get("reactions", [])) > min_reactions_length:
             message["reactions_length"] = len(message["reactions"])
             all_reacted_messages.append(message)
-    return sorted(
+    sorted_all_reacted_messages = sorted(
         all_reacted_messages, key=operator.itemgetter("reactions_length"), reverse=True
     )
+    return sorted_all_reacted_messages
 
 
 def find_most_and_least_reactive_participant(all_reacted_messages, participants):
-    times_reacted = {}
-    for participant in participants:
-        times_reacted[participant] = 0
-        for message in all_reacted_messages:
-            for reaction in message["reactions"]:
-                if reaction["actor"] == participant:
-                    times_reacted[participant] += 1
+    times_reacted = {participant: 0 for participant in participants}
+    for message in all_reacted_messages:
+        for reaction in message.get("reactions", []):
+            actor = reaction["actor"]
+            if actor in participants:
+                times_reacted[actor] += 1
 
     times_reacted = dict(
         sorted(times_reacted.items(), key=lambda item: item[1], reverse=True)
@@ -39,30 +39,29 @@ def find_most_and_least_reactive_participant(all_reacted_messages, participants)
 
 def find_most_likely_to_react_with(all_reacted_messages, emojis, reaction_counter):
     for message in all_reacted_messages:
-        for reaction in message["reactions"]:
+        for reaction in message.get("reactions", []):
             if reaction["reaction"] in emojis:
-                try:
-                    reaction_counter[reaction["actor"]] += 1
-                except KeyError:
-                    reaction_counter[reaction["actor"]] = 0
-                    reaction_counter[reaction["actor"]] += 1
+                actor = reaction["actor"]
+                reaction_counter[actor] = reaction_counter.get(actor, 0) + 1
 
-    return dict(
+    sorted_reaction_counter = dict(
         sorted(reaction_counter.items(), key=lambda item: item[1], reverse=True)
     )
+    return sorted_reaction_counter
 
 
 def find_most_common_reaction(all_reacted_messages):
     emoji_counter = {}
-    for message in all_reacted_messages:
-        for reaction in message["reactions"]:
-            try:
-                emoji_counter[reaction["reaction"]] += 1
-            except KeyError:
-                emoji_counter[reaction["reaction"]] = 0
-                emoji_counter[reaction["reaction"]] += 1
 
-    return dict(sorted(emoji_counter.items(), key=lambda item: item[1], reverse=True))
+    for message in all_reacted_messages:
+        for reaction in message.get("reactions", []):
+            emoji = reaction["reaction"]
+            emoji_counter[emoji] = emoji_counter.get(emoji, 0) + 1
+
+    sorted_emoji_counter = dict(
+        sorted(emoji_counter.items(), key=lambda item: item[1], reverse=True)
+    )
+    return sorted_emoji_counter
 
 
 def find_each_participants_biggest_fan(all_reacted_messages):
@@ -135,6 +134,14 @@ def who_didnt_react(reacted_message, participants):
     return reactors
 
 
+def get_trimmed_reacted_messages(all_reacted_messages, top_results, participants):
+    trimmed_reacted_messages = []
+    for message in all_reacted_messages[:top_results]:
+        message["who_didnt_react"] = who_didnt_react(message, participants)
+        trimmed_reacted_messages.append(message)
+    return trimmed_reacted_messages
+
+
 def main(
     data_to_parse,
     date_range_start,
@@ -143,35 +150,54 @@ def main(
     saveallreacts,
     saveallemojis,
 ):
+    # Get messages and participants
     messages, participants = get_data_to_parse(
         data_to_parse, date_range_start, date_range_end
     )
+
+    # Initialize reaction counter
     reaction_counter = initialise_counter_dict(participants)
 
-    reaction_data = {}
+    # Get all reacted messages
     all_reacted_messages = get_all_reacted_messages(messages)
+
+    # Save all reacted messages or trim and save top results
+    reaction_data = {}
     if saveallreacts:
         reaction_data["reacted_messages"] = all_reacted_messages
     else:
         top_results = 5
-        trimmed_reacted_messages = []
-        for message in all_reacted_messages[:top_results]:
-            message["who_didnt_react"] = who_didnt_react(message, participants)
-            trimmed_reacted_messages.append(message)
+        trimmed_reacted_messages = get_trimmed_reacted_messages(
+            all_reacted_messages, top_results, participants
+        )
         reaction_data[f"top_{top_results}_reacted_messages"] = trimmed_reacted_messages
+
+    # Find most and least reactive participant
     reaction_data["times_reacted"] = find_most_and_least_reactive_participant(
         all_reacted_messages, participants
     )
+
+    # Get emojis to search
     emojis = get_emjois_to_search(emojis_key)
+
+    # Find most likely to react with
     reaction_data[f"reaction_counter_{emojis_key}"] = find_most_likely_to_react_with(
         all_reacted_messages, emojis, reaction_counter
     )
+
+    # Sum of reaction counter values
     reaction_data[f"reaction_counter_{emojis_key}_sum"] = sum(
         reaction_data[f"reaction_counter_{emojis_key}"].values()
     )
+
+    # Save all emojis or get most common reactions
     if saveallemojis:
         reaction_data["emoji_counter"] = find_most_common_reaction(all_reacted_messages)
+
+    # Find each participant's biggest fan
     reaction_data["fan_zone"] = find_each_participants_biggest_fan(all_reacted_messages)
+
+    # Write results to file
     write_to_file("reactionator_results.json", reaction_data)
 
 

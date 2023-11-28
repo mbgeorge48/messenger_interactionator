a37e291c665f8d92a024c58958e2606232b2acd2
Matt George <mbgeorge48@gmail.com>
2023-11-28 19:33:10 +0000

    Add usage info

--- src/utils/get_your_name.py
+++ src/utils/get_your_name.py
@@ -1,5 +1,13 @@
 from .load_json import load_json
 
 
+"""
+Usage
+create a file in the root of the project called `your_name.json`
+And populate it with something like:
+["Gregory Biscuit"]
+"""
+
+
 def get_your_name():
-    return load_json("utils/your_name.json")["name"]
+    return load_json("your_name.json")

6950ef138b2dd1fc4272ba1050132aa6d9f7ab0b
Matt George <mbgeorge48@gmail.com>
2024-05-12 11:42:46 +0100

    Named function params

--- src/utils/write_to_file.py
+++ src/utils/write_to_file.py
@@ -1,7 +1,7 @@
 import json
 
 
-def write_to_file(file_name, data):
+def write_to_file(*, file_name, data):
     with open(file_name, "w") as f:
         json.dump(data, f, indent=4, separators=(",", ": "), ensure_ascii=False)
     print(f"written to {file_name}")
